aac23d2b4a99a94982d0cac56beab84b4d39ccf8
guichitos <92882295+guichitos@users.noreply.github.com>
2025-11-27 09:56:35 -0400

    Update account deletion messaging

--- bot.py
+++ bot.py
@@ -1,9 +1,20 @@
 # bot.py
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from handlers import (
-    start, registrar, stop, ayuda, menu,
+    start,
+    registrar,
+    stop,
+    ayuda,
+    menu,
+    eliminar_cuenta,
     habilidades,
-    comandos_invalidos
+    agregar,
+    eliminar,
+    limpiar,
+    confirmar_eliminar,
+    confirmar_limpiar,
+    confirmar_eliminar_cuenta,
+    comandos_invalidos,
 )
 
 TOKEN = "8017150739:AAGb1UzPk9mWdY5GIfCh2pwLi6J1_NY4Kvk"
@@ -16,7 +27,14 @@ def run_bot():
     app.add_handler(CommandHandler("registrar", registrar))
     app.add_handler(CommandHandler("stop", stop))
     app.add_handler(CommandHandler("ayuda", ayuda))
+    app.add_handler(CommandHandler(["eliminar_cuenta", "borrar"], eliminar_cuenta))
     app.add_handler(CommandHandler("habilidades", habilidades))
+    app.add_handler(CommandHandler("agregar", agregar))
+    app.add_handler(CommandHandler("eliminar", eliminar))
+    app.add_handler(CommandHandler("limpiar", limpiar))
+    app.add_handler(CommandHandler("confirmar_eliminar", confirmar_eliminar))
+    app.add_handler(CommandHandler("confirmar_limpiar", confirmar_limpiar))
+    app.add_handler(CommandHandler(["confirmar_eliminar_cuenta", "confirmar_borrar"], confirmar_eliminar_cuenta))
     app.add_handler(MessageHandler(filters.COMMAND, comandos_invalidos))
 
     print("Bot iniciado. Esperando comandos...")
