6cdfe90e24fd4cc3924320799cff789e3fb24265
guichitos <92882295+guichitos@users.noreply.github.com>
2025-12-12 10:41:30 -0400

    Rename database schema to English names

--- models.py
+++ models.py
@@ -1,9 +1,16 @@
 # models.py
-from dataclasses import dataclass
-from typing import Optional
+from dataclasses import dataclass, field
+from typing import List, Optional, TypedDict
+
+
+class Skill(TypedDict, total=False):
+    name: str
+    slug: str
+    href: str
 
 @dataclass
 class Project:
     Title: str
     Description: Optional[str]
     Url: str
+    Skills: List[Skill] = field(default_factory=list)
